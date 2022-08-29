61a6c88cfd0310c2728dd5e3010fd4173e553254
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2022-08-29 15:53:44 +0600

    two_pointer

--- Array_sum.cpp
+++ Array_sum.cpp
@@ -1,17 +1,21 @@
-#include<bits/stdc++.h>
+ #include<bits/stdc++.h>
  #define nl endl
  using namespace std;
- bool call(int ara[],int n,int sum){
+ vector<int> call(int ara[],int n,int sum){
     int i = 0;
     int j = n -1;
-
+    vector<int>v;
     while(i<j){
         int b = ara[i]+ara[j];
-        if(b == sum ) return true ;
+        if(b == sum ){
+            v.push_back(i);
+            v.push_back(j);
+            return v;
+        }
         else if(b> sum) j--;
         else i++;
     }
-    return false;
+    return v;
  }
  int main(){
          int n,item_sum;
@@ -21,9 +25,11 @@
         for(int i=0 ; i< n; i ++){
                 cin >> ara[i] ;
         }
-
         cin >> item_sum ;
-        if (call(ara,n,item_sum)) cout << "True line !!"<<nl;
+        vector<int>v;
+        v=call(ara,n,item_sum);
+       
+        if (v.size() != 0) cout << "True line !!"<<nl<<v[0]<<" "<<v[1]<<nl;
         else cout<<"False line !!"<<nl;
          return 0;
  }
